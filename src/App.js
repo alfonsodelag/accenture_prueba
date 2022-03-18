01e9c2e212c2f636b9aece06e1c188222af61feb
Alfonso <alfonsodelag1@gmail.com>
2022-03-18 18:20:11 +0100

    creating login and home views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  }
+
   return (
-    <h1 className="font-semibold">
-      Hello world!
-    </h1>
+    <>
+      {!isLoggedIn && <Login onLogin={loginHandler} />}
+      {isLoggedIn && <Home onLogout={logoutHandler} />}
+    </>
   );
 }
 
